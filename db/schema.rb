0767a4ff54b932ad22303b51925d5c552131bd80
Sal Espinosa <sespinos@gmail.com>
2016-04-07 00:31:26 -0600

    Complete iteration 4: add image uploads and minor SASS styling

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160407050235) do
+ActiveRecord::Schema.define(version: 20160407061111) do
 
   create_table "article_tags", force: :cascade do |t|
     t.integer  "tag_id"
@@ -26,8 +26,12 @@ ActiveRecord::Schema.define(version: 20160407050235) do
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
